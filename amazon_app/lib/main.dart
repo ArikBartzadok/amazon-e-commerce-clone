5ed1fc429c12e17ca0f0909565992f2cd33d83b8
Diogo Ferreira <diogocraft.2@gmail.com>
2022-11-08 22:46:27 -0300

    feat(app): `MyApp` implementado estilo global, e gerador de rotas na base da aplicação

--- amazon_app/lib/main.dart
+++ amazon_app/lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:amazon_app/constants/global_variables.dart';
+import 'package:amazon_app/features/auth/screens/auth_screen.dart';
+import 'package:amazon_app/router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +13,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Amazon clone',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Text('Teste'),
-    );
+        title: 'Amazon clone',
+        theme: ThemeData(
+            scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+            colorScheme: const ColorScheme.light(
+              primary: GlobalVariables.secondaryColor,
+            ),
+            appBarTheme: const AppBarTheme(
+                elevation: 0, iconTheme: IconThemeData(color: Colors.black))),
+        onGenerateRoute: (settings) => generateRoute(settings),
+        home: const AuthScreen()
+        // home: Scaffold(
+        //   appBar: AppBar(title: const Text('Titulo')),
+        //   body: Column(children: [
+        //     const Center(
+        //       child: Text('Meio'),
+        //     ),
+        //     Builder(builder: (context) {
+        //       return ElevatedButton(
+        //         onPressed: () {
+        //           Navigator.pushNamed(context, AuthScreen.routeName);
+        //         },
+        //         child: const  Text('click')
+        //       );
+        //     })
+        //   ],)
+        // ),
+        );
   }
 }
